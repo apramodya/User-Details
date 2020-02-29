1e72d623fe8dab91ef18eaf3c5bcdc528b9e79b4
Pramodya <pramodyaabeysinghe@gmail.com>
2020-03-01 04:47:22 +0530

    create and write to a file

--- UserDetails/FormMain.Designer.cs
+++ UserDetails/FormMain.Designer.cs
@@ -30,15 +30,16 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(112, 113);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(224, 217);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(296, 71);
+            this.button1.Size = new System.Drawing.Size(592, 137);
             this.button1.TabIndex = 0;
             this.button1.Text = "Enter your details";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,23 +48,36 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(112, 207);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(224, 398);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(296, 71);
+            this.button2.Size = new System.Drawing.Size(592, 137);
             this.button2.TabIndex = 1;
             this.button2.Text = "Review your details";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.reviewDetails);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(224, 583);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(592, 137);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "File test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.fileTest);
+            // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 353);
+            this.ClientSize = new System.Drawing.Size(948, 783);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormMain";
             this.Text = "Your details";
             this.Load += new System.EventHandler(this.FormMain_Load);
@@ -75,6 +89,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- UserDetails/FormMain.cs
+++ UserDetails/FormMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,6 +39,19 @@ namespace UserDetails
         private void FormMain_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void fileTest(object sender, EventArgs e)
+        {
+            string filePath = String.Format("{0}\\{1}", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "pramodya.txt");
+            FileStream myFile = new FileStream(filePath, FileMode.Append, FileAccess.Write);
+
+            StreamWriter sw = new StreamWriter(myFile);
+            sw.WriteLine("pramodya");
+
+            sw.Flush();
+            sw.Close();
+            myFile.Close();
         }
     }
 }
